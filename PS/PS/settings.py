5659c63d7161e8988d9c8508e1152c23ddfd02d0
Harsh Chauhan <harshchauhan@Harshs-MacBook-Air.local>
2025-10-18 02:59:38 +0530

    Done

--- PS/PS/settings.py
+++ PS/PS/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eh4r*^0l84lip=krc8jt27ay1$me#*y6a18tc1t^xv6w7n5d9k'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
+
 
 
 # Application definition
